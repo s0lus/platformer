ba3be45407f93d23abc5d4eb3024cceb75052f47
s0lus <mashenkoalex@gmail.com>
2019-04-19 21:00:12 +0300

    [Cleanup] Remove unused code

--- Titan/src/Entity/Config.h
+++ Titan/src/Entity/Config.h
@@ -22,10 +22,4 @@ const float climbDownSpeed = 0.05;
 //Начальная скорость при прыжке персонажа
 const float jumpBoost = 0.35;
 
-//Скорость полета пули персонажа
-const float bulletSpeed = 0.2;
-
-//Интервал, с которым стреляет противник
-const int shootInterval = 300;
-
 #endif // CONFIG
